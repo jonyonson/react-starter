4f124c606e027f880c29eaf50000a8233aff0b3d
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-10-26 15:31:33 -0500

    refresh

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'sanitize.css/sanitize.css';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const mountNode = document.getElementById('app');
+ReactDOM.render(<App />, mountNode);
